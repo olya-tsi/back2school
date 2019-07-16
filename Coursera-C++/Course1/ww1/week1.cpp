c69c1e0a627b2c0136a68b1080ada26a91d8df61
Olya <otsirule@gmail.com>
2019-07-16 11:50:06 +0300

    Add all solutions for assignments on week 1
    + solution for division
    + solution for cost calculation
    + solution for even numbers
    + solution for second entry of 'f' in string
    + solution for finding greatest common divisor
    + solution for binary representation of number
    + adding parsing cmd line parameters

--- Coursera-C++/Course1/ww1/week1.cpp
+++ Coursera-C++/Course1/ww1/week1.cpp
@@ -91,13 +91,295 @@ void equation()
     }
 }
 
-int main()
+/* Division assignment
+Дано два натуральных числа A и B, не превышающих 1 000 000. Напишите программу, которая вычисляет целую часть частного от деления A на B.
+
+Если B = 0, выведите "Impossible".
+
+Пример
+
+| stdin | stdout     |
+| ----- | ------     |
+| 10 2	| 5          |
+| 3 5   | 0          |
+| 11 0  | Impossible |
+*/
+void divison()
 {
-    // sum();
+    int a = 0, b = 0;
 
-    // min_string();
+    std::cout << "Enter 2 positive integer numbers:\n";
+    std::cin >> a >> b;
+    if (b > 0 && a >= 0)
+    {
+        std::cout << "The result is " << a / b << std::endl;
+    }
+    else if (b == 0)
+    {
+        std::cout << "Impossible\n";
+    }
+}
 
-    equation();
+/* Cost calculation assignment
+Написать программу вычисления стоимости покупки с учётом скидки. Скидка в X процентов предоставляется, если сумма покупки больше A рублей, в Y процентов -
+если сумма больше B рублей.
+
+В стандартном вводе содержится пять вещественных чисел, разделённых пробелом: N, A, B, X, Y (A < B) - где N - исходная стоимость товара. Выведите стоимость
+покупки с учётом скидки.
+
+Пример
+
+| stdin              | stdout |
+| -----              | ------ |
+| 100 110 120 5 10   | 100    |
+| 115 110 120 5 10   | 109.25 |
+| 150 110 120 5 12.5 | 131.25 |
+*/
+void cost()
+{
+    double n = 0, a = 0, b = 0, x = 0, y = 0;
+
+    std::cout << "Enter 5 numbers in the following order: current total cost, cost for discount #1, cost for discount #2, discount #1's value, discount #2's value\n";
+    std::cin >> n >> a >> b >> x >> y;
+
+    double cost = n;
+    if (n > b && y > 0)
+    {
+        cost = n * ((100 - y) / 100);
+    }
+    else if (n > a && x > 0)
+    {
+        cost = n * ((100 - x) / 100);
+    }
+
+    std::cout << "Final cost is " << cost << std::endl;
+}
+
+/* Even numbers assignment
+Дано два целых числа A и B (A <= B, A >= 1, B <= 30000). Выведите через пробел все чётные числа от A до B (включительно).
+
+Для проверки целого числа x на чётность используется операция взятия остатка от деления на 2, которая в C++ оформляется с помощью символа "%". Например,
+
+
+int x; <br>
+cin >> x; <br>
+if (x % 2 == 0) { <br>
+  // x - чётное число <br>
+} else { <br>
+  // x - нечётное число <br>
+} <br>
+
+Пример
+
+| stdin | stdout     |
+| ----- | ------     |
+| 1 10  | 2 4 6 8 10 |
+| 2 3   | 2          |
+| 9 11  | 10         |
+*/
+void even_numbers() {
+    int a = 0, b = 0;
+
+    std::cout << "Enter 2 integer numbers between 1 and 30000:\n";
+    std::cin >> a >> b;
+
+    if (a % 2 == 1)
+    {
+        a++;
+    }
+
+    while (a <= b)
+    {
+        std::cout << a << " ";
+        a += 2;
+    }
+}
+
+/* Second entry of 'f' assignment
+Дана строка. Найдите в этой строке второе вхождение буквы f и выведите индекс этого вхождения. Если буква f в данной строке встречается только один раз,
+выведите число -1, а если не встречается ни разу, выведите число -2. Индексы нумеруются с нуля.
+
+Пример
+| stdin   | stdout |
+| -----   | ------ |
+| comfort | -1     |
+| coffee  | 3      |
+| car     | -2     |
+
+*/
+void second_entry()
+{
+    std::string s = "";
+    std::cout << "Enter the string:\n";
+    std::cin >> s;
+    int i = -2; // no 'f' character in string
+    for (auto k = 0; k < s.size(); k++)
+    {
+        if (s[k] == 'f' && i == -2)
+        {
+            i = -1;
+        }
+        else if (s[k] == 'f' && i == -1)
+        {
+            i = k;
+            break;
+        }
+    }
+    std::cout << "Second entry of 'f' character in string " << s << " is " << i << std::endl;
+}
+
+/* Greatest common divisor assignment
+В stdin даны два натуральных числа. Выведите в stdout их наибольший общий делитель.
+
+Пример
+
+| stdin | stdout |
+| ----- | ------ |
+| 25 27 | 1      |
+| 12 16 | 4      |
+| 13 13	| 13     |
+
+*/
+void gcd()
+{
+    int a = 0, b = 0;
+    std::cout << "Enter 2 positive numbers:\n";
+    std::cin >> a >> b;
+
+    int gcd = 0;
+    if (a == b || b == 0)
+    {
+        gcd = a;
+    }
+    else if (a == 0)
+    {
+        gcd = b;
+    }
+    else
+    {
+        // int start = a;
+        // if (a > b)
+        // {
+        //     start = b;
+        // }
+        // for (auto i = start; i >= 1; i--)
+        // {
+        //     if (a % i == 0 and b % i == 0)
+        //     {
+        //         gcd = i;
+        //         break;
+        //     }
+        // }
+        gcd = std::min(a, b);
+        int q = a, r = b;
+        if (q < r)
+        {
+            std::swap(q, r);
+        }
+        while (q % r != 0)
+        {
+            gcd = q % r;
+            q = r;
+            r = gcd;
+        }
+
+    }
+
+    std::cout << "GCD(" << a << "," << b << ") is " << gcd << std::endl;
+}
+
+/* Binary numbers
+На вход дано целое положительное число N. Выведите его в двоичной системе счисления без ведущих нулей.
+
+Пример
+
+| stdin | stdout |
+| ----- | ------ |
+| 5     | 101    |
+| 32    | 100000 |
+| 1     | 1      |
+Подсказка
+* число N можно считывать в переменную типа int
+* в этой задаче вам может пригодиться контейнер vector; напоминаем, что добавление значения x в конец вектора v делается так: v.push_back(x);
+*/
+void to_binary()
+{
+    int n = 0;
+    std::cout << "Enter positive number:\n";
+    std::cin >> n;
+    std::vector<int> bins;
+    while (n / 2 != 0)
+    {
+        bins.push_back(n % 2);
+        n /= 2;
+    }
+    bins.push_back(1);
+    std::reverse(bins.begin(), bins.end());
+    std::cout << "Binary representation: ";
+    for (auto i : bins)
+    {
+        std::cout << i;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (1 == argc)
+    {
+        std::cout << "-s  - A+B assignment\n";
+        std::cout << "-ms - min string assignment\n";
+        std::cout << "-e  - quadratic equation assignment\n";
+        std::cout << "-d  - division assignment\n";
+        std::cout << "-c  - cost calculation assignment\n";
+        std::cout << "-en - even numbers assignment\n";
+        std::cout << "-se - second entry assignment\n";
+        std::cout << "-gd - greatest common divisor assignment\n";
+        std::cout << "-b  - number to binary representation\n";
+        return 0;
+    }
+
+    std::string option(argv[1]);
+    if (option == "-s")
+    {
+        sum();
+    }
+    else if (option == "-ms")
+    {
+        min_string();
+    }
+    else if (option == "-e")
+    {
+        equation();
+    }
+    else if (option == "-d")
+    {
+        divison();
+    }
+    else if (option == "-c")
+    {
+        cost();
+    }
+    else if (option == "-en")
+    {
+        even_numbers();
+    }
+    else if (option == "-se")
+    {
+        second_entry();
+    }
+    else if (option == "-gd")
+    {
+        gcd();
+    }
+    else if (option == "-b")
+    {
+        to_binary();
+    }
+    else
+    {
+        std::cout << "Unknown option\n";
+    }
+
 
     return 0;
 }
